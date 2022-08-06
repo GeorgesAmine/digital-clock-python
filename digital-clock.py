ac3eaeb13a026813df0c63ec98495bca8848f7d3
lamcosuser <lamcosuser@LAMCOSPO573.insa-lyon.fr>
2022-08-06 15:46:58 +0300

    Added date to the interface

--- digital-clock.py
+++ digital-clock.py
@@ -4,9 +4,9 @@
 from tkinter import *
 from tkinter.ttk import *
 
-# importing strftime to
-# retreive system's time
-from time import strftime
+# importing date to retrieve 
+# today's date and time
+from datetime import datetime
 
 # creating tkinter window
 root = Tk()
@@ -15,19 +15,37 @@ root.title('Digital Clock')
 # this function is used to display
 # time on the label
 def time():
-    string = strftime('%H:%M:%S %p')
-    lbl.config(text = string)
-    lbl.after(1000,time)
+    now = datetime.now()
+    string = now.strftime('%H:%M:%S %p')
+    lbl_time.config(text = string)
+    lbl_time.after(1000,time)
+
+# this function is used to display
+# today's date me on the label
+def date():
+    today = datetime.today()
+    string = today.strftime("%B %d, %Y")
+    lbl_date.config(text = string)
 
 # Styling the label widget so that clock
 # will look more attractive
-lbl = Label(root, 
+lbl_time = Label(root, 
+            font= ('calibri', 40,'bold'),
+            background = 'white',
+            foreground = 'black')
+
+# Styling the label widget so that date
+# will look more attractive
+lbl_date = Label(root, 
             font= ('calibri', 40,'bold'),
             background = 'white',
             foreground = 'black')
 
 # Placing clock at the center of tkinter window
-lbl.pack(anchor = 'center')
+lbl_time.pack(anchor = 'center')
+# Placing date on the bottom
+lbl_date.pack(anchor = 's')
 time()
+date()
 
 mainloop()
